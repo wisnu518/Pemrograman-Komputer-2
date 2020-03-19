4cf2ddd0910c3b26690ce16b057dab46c9bdd586
wisnu518 <wisnuwisnu466@gmail.com>
2020-03-19 17:29:31 +0700

    wisnu

---
+++ src/serialisasi/ProductItem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serialisasi;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Asus
+ */
+public class ProductItem implements Serializable {
+
+    String nama;
+    double harga;
+
+    public ProductItem(String name, double price) {
+        this.nama = name;
+        this.harga = price;
+    }
+
+    @Override
+    public String toString() {
+        return "\r\nNama=" + nama + "\r\nHarga=" + harga + "\r\n";
+    }
+}
